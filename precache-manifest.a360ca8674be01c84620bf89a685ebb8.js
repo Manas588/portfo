1051b0afa3b477c0f9b90142cafeaff0dcd4d7c7
Manas Kulkarni <61631964+Manas588@users.noreply.github.com>
2020-05-22 17:07:38 +0530

    Updates

---
+++ precache-manifest.a360ca8674be01c84620bf89a685ebb8.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2375199466cf88aaff6cb6854b6ceb32",
+    "url": "/portfo/index.html"
+  },
+  {
+    "revision": "830e693c72d51a3b6b6f",
+    "url": "/portfo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "64682a197d1ab970238c",
+    "url": "/portfo/static/css/main.f80281fb.chunk.css"
+  },
+  {
+    "revision": "830e693c72d51a3b6b6f",
+    "url": "/portfo/static/js/2.5af01e44.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/portfo/static/js/2.5af01e44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64682a197d1ab970238c",
+    "url": "/portfo/static/js/main.8c973912.chunk.js"
+  },
+  {
+    "revision": "045e57d21fedfd840c9a",
+    "url": "/portfo/static/js/runtime-main.f6183858.js"
+  },
+  {
+    "revision": "e68a69a432eb890cb81b44631d96c2e6",
+    "url": "/portfo/static/media/Algo.e68a69a4.jpg"
+  },
+  {
+    "revision": "4fe7dbaf812550188fee702dd97dbe60",
+    "url": "/portfo/static/media/CSS.4fe7dbaf.png"
+  },
+  {
+    "revision": "15a4866e67b5a9aac43ae3af08de6721",
+    "url": "/portfo/static/media/JS.15a4866e.png"
+  },
+  {
+    "revision": "b47fa61d2fedcf51fa74fd6de126d082",
+    "url": "/portfo/static/media/ML.b47fa61d.jpg"
+  },
+  {
+    "revision": "22eeecf99b8d92726b4f3735be5473c0",
+    "url": "/portfo/static/media/PY.22eeecf9.png"
+  },
+  {
+    "revision": "1d602fdecd6bc54991b4a186e489fbe0",
+    "url": "/portfo/static/media/Py.1d602fde.jpg"
+  },
+  {
+    "revision": "7197c4a9b4d8610b2ae1f1055b174f96",
+    "url": "/portfo/static/media/WD.7197c4a9.jpg"
+  },
+  {
+    "revision": "d5eebc6eeab7148ecb969bd00b2ca283",
+    "url": "/portfo/static/media/cap.d5eebc6e.png"
+  },
+  {
+    "revision": "2c1a7560c88ea83e6b2593cd07af8ad8",
+    "url": "/portfo/static/media/email.2c1a7560.png"
+  },
+  {
+    "revision": "3d7af0fa2eb37c8d7b88a212e155fb68",
+    "url": "/portfo/static/media/github-logo.3d7af0fa.png"
+  },
+  {
+    "revision": "e8e69ed0705cd96f6f147e123612ae05",
+    "url": "/portfo/static/media/instagram.e8e69ed0.png"
+  },
+  {
+    "revision": "3f52a778db4b741e7773eb753565a1f6",
+    "url": "/portfo/static/media/location.3f52a778.png"
+  },
+  {
+    "revision": "d812e260452c8745a26533c7bda0b806",
+    "url": "/portfo/static/media/logo.d812e260.png"
+  },
+  {
+    "revision": "c7250eb4d5d8ee7900616a5b1ccf3c54",
+    "url": "/portfo/static/media/matlab.c7250eb4.png"
+  },
+  {
+    "revision": "4b89fce7bc31e2146fdb42e6867fee35",
+    "url": "/portfo/static/media/mlprojs.4b89fce7.jpg"
+  },
+  {
+    "revision": "528234aa3ae3f5f644c63921cab09659",
+    "url": "/portfo/static/media/node.528234aa.png"
+  },
+  {
+    "revision": "98c709f25fb37821ac0fd06fd728984e",
+    "url": "/portfo/static/media/python-proj.98c709f2.jpg"
+  },
+  {
+    "revision": "70f87bba34487dc1c0cb8916d800c63b",
+    "url": "/portfo/static/media/react.70f87bba.png"
+  },
+  {
+    "revision": "09ba0105b3bc11dac5b7c09443812189",
+    "url": "/portfo/static/media/skl.09ba0105.png"
+  },
+  {
+    "revision": "8e4e44c5eb6bc0cf86fd70a212051329",
+    "url": "/portfo/static/media/sql.8e4e44c5.png"
+  },
+  {
+    "revision": "13078d3724fd881a36204970293c49d1",
+    "url": "/portfo/static/media/udemy-logo.13078d37.png"
+  },
+  {
+    "revision": "61bcb4ed0ad07abbfeb859ed0a5fdc75",
+    "url": "/portfo/static/media/web-app.61bcb4ed.png"
+  }
+]);
